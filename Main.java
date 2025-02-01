73f0c49e4622cabc71ffa66dba0336e5caa4350c
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-02-01 23:07:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+//Main.java
+//Name : Karan Rekhan
+//PRN : 23070126054
+//Batch : 2023-2027
+import java.util.* ;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        UserInput userInput = new UserInput(scan);
+        Calculator calculator = new Calculator();
+        
+       
